816e11f3a41e74937433572615af3263c20594c1
boytalaba <36549438+boytalaba@users.noreply.github.com>
2022-05-29 20:23:21 +0800

    Add files via upload

---
+++ config/index.js
@@ -0,0 +1,11 @@
+export const config = {
+  env: "prod",
+  LOCAL_API_URL: "localhost:3000",
+  API_URL: "odp-rarity.vercel.app",
+  COLLECTION_NAME: "Chickelon",
+  COLLECTION_TITLE: "Chickelon NFT",
+  COLLECTION_DESCRIPTION: '2222 generated NFT',
+  COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
+  STARTING_INDEX: 0,
+  WEIGHTS: {},
+};
